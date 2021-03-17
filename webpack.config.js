7b6375bc19304c374a3bebe188d210b78bde6be4
skythrilla <schuyler.lawrence@gmail.com>
2021-03-16 21:39:07 -0700

    set mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 module.exports = {
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader"
-      }
-    ]
-  }
+        loader: 'babel-loader',
+      },
+    ],
+  },
 };
