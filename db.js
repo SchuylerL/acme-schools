ab23375d44c15d3983a64e24a20124cd93aa49a4
skythrilla <schuyler.lawrence@gmail.com>
2021-03-29 12:01:26 -0700

    2

--- db.js
+++ db.js
@@ -5,10 +5,10 @@ const conn = new Sequelize(
   {
     dialect: 'postgres',
     protocol: 'postgres',
-    dialectOptions: {
+    /* dialectOptions: {
       ssl: true,
       rejectUnauthorized: false,
-    },
+    }, */
   }
 );
 
